e76572737414444293b7c09aa8b8429673a3fb4d
vorachith phoubandith <songta17@hotmail.com>
2021-04-23 18:51:49 +0700

    Jest 1

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,26 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+// import { shallowMount } from '@vue/test-utils'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
+// describe('HelloWorld.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message'
+//     const wrapper = shallowMount(HelloWorld, {
+//       props: { msg }
+//     })
+//     expect(wrapper.text()).toMatch(msg)
+//   })
+// })
+
+import { mount } from '@vue/test-utils'
+
+const App = {
+  template: `
+    <div>Hello World</div>
+  `
+}
+
+test('App', function() {
+  const wrapper = mount(App)
+  console.log(wrapper.html())
+  expect(wrapper.html()).toBe('<div>Hello World</div>')
 })
